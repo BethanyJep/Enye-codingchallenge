0bc34cb95e23f1c6bfdb3f065559454df48a168e
[InnCreator] <[engbethany4@gmail.com]>
2020-06-04 00:08:09 +0300

    locahost:4930

---
+++ hospital-locator/src/App.tsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import React, { FC } from 'react';
+import { Button } from 'antd';
+import './App.css';
+
+const App: FC = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+);
+
+// export default App;
+
+export default App;
